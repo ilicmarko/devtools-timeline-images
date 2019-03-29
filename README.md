567f36e0ffe610f956fc6415a16e068557aa4fab
Maverick <ilic.marko.05@live.com>
2019-03-29 11:54:58 +0100

    chore(*): init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Devtools Timeline Images
+
+A really small utility to extract images from Chrome Timeline.
+
+## Setup
+
+```bash
+yarn global add devtools-timeline-images
+# or NPM
+npm i -g devtools-timeline-images
+```
+
+## Usage
+
+CLI takes to arguments `-i` and `-o`:
+* `-i` or `--input` - Specify the input JSON file.
+* `-o` or `--output` - Specify the output folder.
+
+*Note: If the output directory does not exist the CLI will create it.*
+
+## Example
+
+```bash
+devtools-timeline-images -i ./example-site.json -o ./images
+```
+
+## Save a recoding
+
+![Save recoding](https://developers.google.com/web/tools/chrome-devtools/evaluate-performance/imgs/save-profile.png)
+
+More information at [Google Developer Docs](https://developers.google.com/web/tools/chrome-devtools/evaluate-performance/reference#save)
---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2), {
+    string: ['input', 'output'],
+    alias: { i: 'input', o: 'output' },
+});
+
+if (!argv.hasOwnProperty('i')) {
+    console.error(chalk.red('`--input` or `-i` argument required'));
+    process.exit(1);
+}
+
+if (!argv.hasOwnProperty('o')) {
+    console.error(chalk.red('`--output` or `-o` argument required'));
+    process.exit(1);
+}
+const { input, output } = argv;
+
+if (!fs.existsSync(input)) {
+    console.error(chalk.red(`Provided file '${input}' doesn't exist`));
+    process.exit(1);
+}
+
+if (!fs.existsSync(output)) {
+    try {
+        fs.mkdirSync(output);
+        console.log(chalk.blue(`Directory ${output} created!`));
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error(`EACCES: permission denied, mkdir '${output}'`);
+        }
+
+        throw err;
+    }
+}
+
+const fileData = fs.readFileSync(input, 'utf8');
+let jsonData;
+
+try {
+    jsonData = JSON.parse(fileData);
+} catch (e) {
+    console.error(chalk.red(e));
+    process.exit(1)
+}
+
+if (!Array.isArray(jsonData)) {
+    console.log('This doesn\'t look like Chrome Timeline JSON. Please provide valid data.');
+    process.exit(1);
+}
+
+const snapshots = jsonData.filter(item => item.name === 'Screenshot').map(item => item.args.snapshot);
+
+if (!snapshots.length) {
+    console.error(chalk.red('There are no captured frames inside the provided input.'));
+    process.exit(1);
+}
+
+snapshots.forEach((snapshot, i) => {
+    fs.writeFileSync(path.resolve(output, `${i}.png`), Buffer.from(snapshot, 'base64'));
+});
+
+console.log(chalk.green.bold(`Files exported to ${output}`));
+process.exit();
